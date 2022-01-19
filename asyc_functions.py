3ceab95cdaeb4931b723d708749951d1704d9a92
Михайло Лазорик <mikolaz2727@gmail.com>
2022-01-19 22:18:45 +0200

    Added new file

---
+++ asyc_functions.py
@@ -0,0 +1,19 @@
+def concat_sequence_v1(s1, s2):
+    for elem in s1:
+        yield elem
+    for elem in s2:
+        yield elem
+
+
+def concat_sequence_v2(s1, s2):
+    yield from s1
+    yield from s2
+
+
+seq1 = range(10)
+seq2 = range(10, 20)
+result = concat_sequence_v2(seq1, seq2)
+
+for i in result:
+    print(i)
+
